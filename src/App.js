fc6fcf28a041d232d02d299e3a426ad9ac4823e5
code0xff <ian.jungyong.um@gmail.com>
2023-02-14 22:53:32 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { evmToAddress } from '@polkadot/util-crypto'; 
+import React, { useState } from 'react';
+
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const [color, setColor] = useState('white');
+
+  const convert = () => {
+    try {
+      setColor('white')
+      setOutput(evmToAddress(input.trim()))
+    } catch(e) {
+      setColor('darkred')
+      setOutput(e.message)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h3 style={{color: 'white'}}>Ethereum Address Converter for Frontier</h3>
+        <br/>
+        <br/>
+        <input type='text' className='input-default' placeholder='Ethereum Address' onChange={(e) => setInput(e.target.value)}/>
+        <br/>
+        <button className='button-default' onClick={convert}>Convert</button>
+        <br/>
+        <input type='text' className='input-default' readOnly value={output} style={{color: color}}/>
+      </div>
     </div>
   );
 }
