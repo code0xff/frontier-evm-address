d1a542ddd0c147d55b445b3ec08fa0e804e615ff
code0xff <ian.jungyong.um@gmail.com>
2023-02-15 12:06:34 +0800

    add substrate address to ethereum address for withdraw

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 
-import { evmToAddress } from '@polkadot/util-crypto'; 
+import { addressToEvm, evmToAddress } from '@polkadot/util-crypto'; 
 import React, { useState } from 'react';
 
 function App() {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
   const [color, setColor] = useState('white');
+  const [mode, setMode] = useState('evmToAddress');
 
   const convert = () => {
     try {
       setColor('white')
-      setOutput(evmToAddress(input.trim()))
+      setOutput(mode === 'evmToAddress' ? evmToAddress(input.trim()): `0x${[...addressToEvm(input.trim())].map(x => x.toString(16).padStart(2, '0'))
+      .join('')}`)
     } catch(e) {
       setColor('darkred')
       setOutput(e.message)
@@ -23,8 +25,12 @@ function App() {
       <div className="App-header">
         <h3 style={{color: 'white'}}>Ethereum Address Converter for Frontier</h3>
         <br/>
+        <select className='select-default' onChange={(e) => setMode(e.target.value)}>
+          <option value={'evmToAddress'}>Ethereum Address to Substrate Address for Deposit</option>
+          <option value={'addressToEvm'}>Substrate Address to Ethereum Address for Withdraw</option>
+        </select>
         <br/>
-        <input type='text' className='input-default' placeholder='Ethereum Address' onChange={(e) => setInput(e.target.value)}/>
+        <input type='text' className='input-default' placeholder={mode === 'evmToAddress' ? 'Ethereum Address' : 'Substrate Address'} onChange={(e) => setInput(e.target.value)}/>
         <br/>
         <button className='button-default' onClick={convert}>Convert</button>
         <br/>
